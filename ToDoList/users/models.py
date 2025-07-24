ba40845f73dbe2418051125fd67488f5aa608b9e
Ayxan-T <ayxan.tagiyev14@gmail.com>
2025-07-24 14:55:56 +0400

    start users app and define User model

---
+++ ToDoList/users/models.py
@@ -0,0 +1,9 @@
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.TextField(blank=True)
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(validators=[MinLengthValidator(6)])
