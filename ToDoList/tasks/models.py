dcba221415bc52549a1a949872800f38eb5a29bb
Ayxan-T <ayxan.tagiyev14@gmail.com>
2025-07-24 15:24:39 +0400

    start tasks app and define Task model

---
+++ ToDoList/tasks/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+class Task(models.Model):
+    status_of_task = [
+        ('new','New'),
+        ('in_progress', 'In Progress'),
+        ('completed','Completed')
+    ]
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices= status_of_task, default='new')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return f"{self.title} ({self.status})"
--- ToDoList/users/models.py
+++ ToDoList/users/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     first_name = models.CharField(max_length=20)
-    last_name = models.TextField(blank=True)
+    last_name = models.TextField(blank=True, null=True)
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(validators=[MinLengthValidator(6)])
+
+    def __str__(self):
+        return f"{self.username}"
