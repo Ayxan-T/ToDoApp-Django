4facda2cc6283967cbee0ff8c1f5ba9053025d7d
Ayxan-T <ayxan.tagiyev14@gmail.com>
2025-07-25 15:02:28 +0400

    fix name

--- ToDoList/tasks/views.py
+++ ToDoList/tasks/views.py
@@ -5,7 +5,7 @@ from .serializers import TaskSerializer
 
 # Create your views here.
 @api_view(['GET'])
-def getData(request):
+def getAllTasks(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
