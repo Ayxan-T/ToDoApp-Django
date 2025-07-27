0ff4e09e966a7250ffbd51d2302ac3c128a28d2a
Ayxan-T <ayxan.tagiyev14@gmail.com>
2025-07-27 14:05:10 +0400

    add login tests

--- ToDoList/users/tests.py
+++ ToDoList/users/tests.py
@@ -4,30 +4,34 @@ from rest_framework import status
 from users.models import User
 from django.urls import reverse
 
+loginURL = reverse('login')
+registerURL = reverse('register')
+
 # Create your tests here.
-class UserTests(TestCase):
+class UserRegistrationTests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-    # Test user registration
     def test_register_user_valid(self):
         data = {
             'first_name': 'Test1',
             'username': 'testuser1',
             'password': 'testpassword'
         }
-        good_response = self.client.post(reverse('register'), data)
+        good_response = self.client.post(registerURL, data)
         self.assertEqual(good_response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.filter(username=data['username']).count(), 1)
+    
     def test_register_user_invalid_password(self):
         data = {
             'first_name': 'Test2',
             'username': 'testuser2',
             'password': 'pwd2'
         }
-        bad_response = self.client.post(reverse('register'), data)
+        bad_response = self.client.post(registerURL, data)
         self.assertEqual(bad_response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.filter(username=data['username']).count(), 0)
+    
     def test_register_user_invalid_username(self):
         data = {
             'first_name': 'Test3',
@@ -38,24 +42,73 @@ class UserTests(TestCase):
         User(first_name='AnotherFirstName', username=data['username'], password='anotherpassword').save()
 
         # Attempt to register again with the same username
-        bad_response = self.client.post(reverse('register'), data)
+        bad_response = self.client.post(registerURL, data)
         self.assertEqual(bad_response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.filter(username=data['username']).count(), 1)
+    
     def test_register_user_missing_fields(self):
         data = {
             'first_name': 'Test4',
             'username': 'testuser4'
             # Missing password
         }
-        bad_response = self.client.post(reverse('register'), data)
+        bad_response = self.client.post(registerURL, data)
         self.assertEqual(bad_response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.filter(username=data['username']).count(), 0)
+    
     def test_register_user_empty_fields(self):
         data = {
             'first_name': '',
             'username': '',
             'password': ''
         }
-        bad_response = self.client.post(reverse('register'), data)
+        bad_response = self.client.post(registerURL, data)
         self.assertEqual(bad_response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.filter(username=data['username']).count(), 0)
+
+class UserLoginTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        User(first_name='TestUser', username='testuser', password='testpassword').save()
+
+    def test_login_user_valid(self):
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        response = self.client.post(loginURL, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+    def test_login_user_invalid_password(self):
+        data = {
+            'username': 'testuser',
+            'password': 'wrongpassword'
+        }
+        response = self.client.post(loginURL, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_login_user_nonexistent(self):
+        data = {
+            'username': 'nonexistentuser',
+            'password': 'somepassword'
+        }
+        response = self.client.post(loginURL, data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_login_user_missing_fields(self):
+        data = {
+            'username': 'testuser'
+            # Missing password
+        }
+        response = self.client.post(loginURL, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_login_user_empty_fields(self):
+        data = {
+            'username': '',
+            'password': ''
+        }
+        response = self.client.post(loginURL, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
